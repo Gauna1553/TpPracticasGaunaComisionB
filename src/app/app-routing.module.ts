ffd36bc648b875e45ab56d7d420c367e5d791d36
Gauna1553 <gabrielgauna04@gmail.com>
2023-07-02 15:39:36 -0300

    creamos el componente addItem

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ItemComponent } from './components/item/item.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: ItemComponent},
+  {path: "/add", component: AddComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
